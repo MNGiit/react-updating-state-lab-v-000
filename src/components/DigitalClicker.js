6e368fea84e8d6a5624aa082ae7e69a85c017831
Mohammed Noori <kmanoorib@gmail.com>
2019-08-19 03:49:57 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class DigitalClicker extends React.Component {
+export default class DigitalClicker extends React.Component {
   constructor() {
     super();
 
@@ -21,5 +21,3 @@ class DigitalClicker extends React.Component {
     );
   }
 }
-
-export default DigitalClicker;
